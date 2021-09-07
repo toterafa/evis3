07f269978d4b34db9bea8bb2ca0ceb29184866e6
toterafa <79848666+toterafa@users.noreply.github.com>
2021-09-07 11:15:29 -0300

    att1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './componentes/Header';
 
 export default function App() {
   
   
   return (
-    <div>
-         <p> Bora começar =)  </p>
-    </div>
+    <>
+    <Header />
+    </>
   );
 };
---
+++ src/componentes/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../css/Header.css'
+
+
+export default ()=>{
+
+
+    return(
+        <>
+            <nav>
+                <label className='logo'>EVI</label>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>Sobre Evince</li>
+                    <li><a href="#"></a>Cadastre-se</li>
+                    <li><a href="#"></a>Contato</li>
+                </ul>
+
+            </nav>
+
+
+        </>
+    );
+
+
+}
